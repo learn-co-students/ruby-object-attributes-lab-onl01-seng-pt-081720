282afb75307e5e6d5c3ce7b78b21ae2667bcf568
Oliver Bebber <ogbebber@gmail.com>
2020-09-01 00:46:03 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+
+  def name=(person_name)
+    @name = "Beyonce"
+  end
+
+  def name
+    @name
+  end
+end
+
+
+class Person
+
+  def job=(person_job)
+    @job = "Singer"
+  end
+
+  def job
+    @job
+  end
+end
