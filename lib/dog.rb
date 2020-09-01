8df5374960605523ef2e92f7f29f943b651242bd
Elizabeth Cobb <cobbeec@gmail.com>
2020-09-01 23:19:36 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,36 @@
+class Dog 
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  
+def name 
+  @name 
+end 
+
+def breed=(breed)
+   @breed = breed
+ end 
+ 
+ def breed
+   @breed 
+end
+end 
+
+class Person 
+ def name=(person_name)
+    @name = person_name
+  end
+  
+def name 
+  @name 
+end 
+
+
+  def job=(job_name)
+    @job = job_name
+  end 
+
+  def job 
+    @job 
+  end 
+end  
