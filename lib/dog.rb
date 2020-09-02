a3032176ebeba84fb1819c2ef7b0bd150476a912
Jazmin Matos <jmatos1993@gmail.com>
2020-09-02 03:26:02 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+  def initialize #initialize
+    @name = name
+  end
+
+  def name=(name) #setter method
+    @name = name
+  end
+
+  def name #getter method
+    @name
+  end
+  
+  def breed=(breed)
+    @breed = breed  
+  end
+
+  def breed
+    @breed
+  end
+
+end
